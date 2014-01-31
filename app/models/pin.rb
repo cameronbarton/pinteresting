5681d2cc35270b6df47c131520ba8cf2ec30b158
Cameron Barton <cameront21@gmail.com>
2014-01-30 20:07:44 -0800

    Change email to name on show page

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,7 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, :styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100>" }
+
+  validates :image, presence: true
+  validates :description, presence: true
 end
